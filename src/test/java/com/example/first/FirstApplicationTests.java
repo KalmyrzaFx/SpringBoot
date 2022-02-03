ae2c4d25ad209e10591e4240e8948b1660a0319f
User <Zholchubekovk@gmail.com>
2022-02-03 10:02:04 +0600

    deploy

--- src/test/java/com/example/first/FirstApplicationTests.java
+++ src/test/java/com/example/first/FirstApplicationTests.java
@@ -2,11 +2,12 @@ package com.example.first;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
+@SpringBootApplication
 @RestController
 class FirstApplicationTests {
 
